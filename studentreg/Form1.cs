229f1e0708578b98f84ad9de8421e3e1ceacc1d3
ayesha31ash <43003827+ayesha31ash@users.noreply.github.com>
2024-04-25 00:03:07 +0530

    Add files via upload

---
+++ studentreg/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Security.Cryptography.X509Certificates;
+
+namespace studentreg
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            load();
+        }
+
+        SqlConnection conn = new SqlConnection("Data Source=Ayesha; Initial Catalog=gcbt; User Id=sa; Password=12345");
+        SqlCommand cmd;
+        SqlDataReader read;
+        SqlDataAdapter drr;
+        string id;
+        bool Mode = true;
+        string sql;
+
+
+        public void load()
+        {
+            try
+            {
+                sql = "select * from student";
+                cmd = new SqlCommand(sql, conn);
+                conn.Open();
+
+                read = cmd.ExecuteReader();
+
+                drr = new SqlDataAdapter(sql, conn);
+                dataGridView1.Rows.Clear();
+
+                while (read.Read())
+                {
+                    dataGridView1.Rows.Add(read[0],read[1],read[2],read[3]);
+
+                }
+                conn.Close();
+
+
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void getID(String id)
+        {
+
+            sql = "select * from student where id = '" + id + "' ";
+
+            cmd = new SqlCommand(sql, conn);
+            conn.Open();
+            read = cmd.ExecuteReader();
+
+            while(read.Read())
+            {
+                txtName.Text = read[1].ToString();
+                txtCourse.Text = read[2].ToString();
+                txtFee.Text = read[3].ToString();
+
+
+            }
+
+            conn.Close();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtName.Clear();
+            txtCourse.Clear();
+            txtFee.Clear();
+            txtName.Focus();
+            button1.Text = "Save";
+            Mode = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = txtName.Text;
+            string course = txtCourse.Text;
+            string fee = txtFee.Text;
+
+            if(Mode == true)
+            {
+                sql = "insert into student(stname,course,fee) values(@stname,@course,@fee)";
+                conn.Open();
+                cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@stname", name);
+                cmd.Parameters.AddWithValue("@course", course);
+                cmd.Parameters.AddWithValue("@fee",fee);
+                MessageBox.Show("Record Addddedddd");
+                cmd.ExecuteNonQuery();
+
+                txtName.Clear();
+                txtCourse.Clear();
+                txtFee.Clear();
+                txtName.Focus();
+                
+            }
+            else
+            {
+                id = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+                sql = "update student set stname = @stname, course = @course, fee = @fee where id = @id";
+                conn.Open();
+                cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@stname", name);
+                cmd.Parameters.AddWithValue("@course", course);
+                cmd.Parameters.AddWithValue("@fee", fee);
+                cmd.Parameters.AddWithValue("@id", id);
+                MessageBox.Show("Record Updatedddd");
+                cmd.ExecuteNonQuery();
+
+                txtName.Clear();
+                txtCourse.Clear();
+                txtFee.Clear();
+                txtName.Focus();
+                button1.Text = "Save";
+                Mode = true;
+
+            }
+            conn.Close();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(e.ColumnIndex == dataGridView1.Columns["Edit"].Index && e.RowIndex>=0) 
+            {
+                Mode = false;
+                id = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+                getID(id);
+                button1.Text = "Edit";
+            
+            }
+            else if(e.ColumnIndex == dataGridView1.Columns["Delete"].Index && e.RowIndex >= 0)
+            {
+                Mode = false;
+                id = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+                sql = "delete from student where id = @id";
+                conn.Open();
+                cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Record Deleted");
+                conn.Close();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            load();
+        }
+    }
+}
